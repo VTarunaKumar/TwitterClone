c5334c9155b273c973f938a1feef1ea3cadd7071
VTarunaKumar <vtarunakumarrao@gmail.com>
2023-02-08 13:04:27 +0530

    Completing Theme Process

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,6 @@
+class AppWriteConstants{
+  static const String databaseId = '63e34acd6fd83424cbe8';
+  static const String projectId = '63e3456690a21d760655';
+  static const String endPoint = 'http://localhost:80/v1';
+
+}
